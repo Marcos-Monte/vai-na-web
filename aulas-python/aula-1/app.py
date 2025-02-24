86cfe04cbc9be7fd888e33343356e7df3b2e665a
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-02-24 19:39:06 -0300

    aula de intro referente ao backend 'python'

---
+++ aulas-python/aula-1/app.py
@@ -0,0 +1,16 @@
+# Tipo numero inteiro = class int
+idade_marcos = 36
+# Tipo numero decimal = class float
+idade_marcos = 36.0
+# Tipo texto = class str
+idade_marcos = '36'
+# Tipo 'validação de verdadeiro ou falso' = class bool (booleano)
+idade_marcos = 36 == 36
+
+# Mostra o tipo de dado inserido na variavel
+#type(variavel)
+
+# Pegar Input do Usuario 
+nome = input('Qual o seu nome? ')
+# Imprimir no console
+print('Seja bem vindo: ', nome)
